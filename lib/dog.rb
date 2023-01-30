2542ab21bb4f4fb2f7955cb94b19b71d5801d25c
Walker Le <walker.le88@gmail.com>
2023-01-29 21:51:48 -0700

    commit tree 8715a88b76acad38276af58c895839e7b45105d2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
+  # setter method
+  def name= dog_name
+    @this_dogs_name = dog_name
+  end
+
+  # getter method
+  def name
+    @this_dogs_name
+  end
+
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
